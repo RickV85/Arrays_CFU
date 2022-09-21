5c04fb981b67a6c54708dc80abbbe86f8399134c
Rick Vermeil <rickv85@gmail.com>
2022-09-21 10:31:56 -0600

    Display age range with slice, create new agePrimary var

--- arrays.js
+++ arrays.js
@@ -9,3 +9,9 @@ console.log(married.shift())
 age.push('37')
 // display age with added '37'
 console.log(age)
+//display second and third elements only (32 and 51) in 'age' array
+console.log(age.slice(1, 3))
+// create a new variable 'agePrimary' from second and third element in 'age'
+var agePrimary = age.slice(1, 3)
+// display new variable 'agePrimary'
+console.log(agePrimary)
