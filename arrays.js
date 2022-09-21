4b71aff901c3baa4bbcba3aa83251b9cbdb2462a
Rick Vermeil <rickv85@gmail.com>
2022-09-21 10:24:32 -0600

    Add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
-var names = ['Alan', 'Bobby', 'Chris', 'Dave']
-var age = ['65', '32', '51', '26']
-var married = ['true', 'false', 'false', 'true']
+var names = ['Alan', 'Bobby', 'Chris', 'Dave'];
+var age = ['65', '32', '51', '26'];
+var married = ['true', 'false', 'false', 'true'];
+// display the last element (Dave) of 'names' variable
+console.log(names.pop())
+// display the first element (true) in married array
+console.log(married.shift())
+// add '37' end of to age array
+age.push('37')
+// display age with added '37'
+console.log(age)
